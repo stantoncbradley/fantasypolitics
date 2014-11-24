32a47f23ad7026414e6f7212bd90e16825bef9c6
Scott Latham <silatham99@gmail.com>
2014-11-24 03:01:45 -0600

    Alter Facebook login/signup logic slightly

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,10 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => 'Facebook') if is_navigational_format?
-    else
+    if @user.nil? || !@user.persisted?
       @user = User.by_email(request.env['omniauth.auth'].info.email).first
 
       unless @user.nil?
@@ -23,9 +20,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
                              })
         CreateDemoTeamService.execute(@user)
       end
-
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => 'Facebook') if is_navigational_format?
     end
+
+    sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    set_flash_message(:notice, :success, :kind => 'Facebook') if is_navigational_format?
   end
 end
