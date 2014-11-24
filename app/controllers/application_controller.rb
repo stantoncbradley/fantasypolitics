6855155286de4115191d0c0d19b59b455e8631f3
Scott Latham <silatham99@gmail.com>
2014-11-23 21:01:29 -0600

    Add support for all FP domains to opengraph url parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :set_working_url
+
+  def set_working_url
+    params[:working_url] ||= request.base_url
+  end
+
   #before_filter :authenticate_user!
 end
