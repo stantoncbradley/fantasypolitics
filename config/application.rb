d98de7792d1ea316c50b9996ec562e3a022aff16
Scott Latham <silatham99@gmail.com>
2014-11-25 23:03:11 -0600

    Add skylight to staging env, use env variables for auth keys

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module FantasyPolitics
 
     config.autoload_paths += %W( #{config.root}/app/**/ )
 
+    config.skylight.environments += ['staging']
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
