9591e7e33824343c637634d2de5ca021c82f5ca0
Scott Latham <silatham99@gmail.com>
2014-11-26 23:47:58 -0600

    Style status displayed in tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
 
   def show
       @eligible_leagues = Team.by_user(current_user.id).invited_or_joined.to_a.map { |t| { league: League.find(t.league_id), team: t } }
+      @pending_trade_count = 0
+
+      current_user.teams.each do |team|
+        @pending_trade_count += team.instigated_trades.to_a.select { |t| t.status == 'Pending' }.count
+        @pending_trade_count += team.offered_trades.to_a.select{ |t| t.status == 'Pending' }.count
+      end
   end
 
   def fb_test
